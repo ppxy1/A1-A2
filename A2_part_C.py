dd857816d4b5acff626611971442fa0892d6cd28
ppxy1 <e1373563@u.nus.edu>
2025-10-22 16:23:43 +0800

    Update A2_part_C.py

--- A2_part_C.py
+++ A2_part_C.py
@@ -152,6 +152,6 @@ if __name__ == "__main__":
     main()
 
 # python A2_c.py \
-#   --train_dir /raid/baiyang/xiaoyu/assignment/ML_test/question2/Training \
-#   --test_dir  /raid/baiyang/xiaoyu/assignment/ML_test/question2/Testing \
+#   --train_dir assignment/ML_test/question2/Training \
+#   --test_dir  assignment/ML_test/question2/Testing \
 #   --img_size 150 --epochs 10 --bs 16 --sampler 1 --use_class_weight 1
